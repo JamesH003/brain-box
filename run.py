69d93097a60d10dc8401e9f54ba38c079a13f894
James Horgan <jamesrhorgan@gmail.com>
2023-06-16 15:38:53 +0000

    Add questions to QUESTION_OPTIONS dictionary

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
 QUESTION_OPTIONS = {
-    
+    "What is the capital city of Portugal?": [
+        "Lisbon", "Caracas", "Faro", "Ottawa"
+    ],
+    "What is the capital city of Denmark?": [
+        "Copenhagen", "Moscow", "Tokyo", "Stockholm"
+    ],
+    "What is the capital city of Cuba?": [
+        "Havana", "Jakarta", "Cairo", "Miami"
+    ],
+    "What is the capital city of Argentina?": [
+        "Buenos Aires", "Budapest", "Monrovia", "Maseru"
+    ],
+    "What is the capital city of Croatia?": [
+        "Zagreb", "Accra", "Damascus", "Split"
+    ],
+    "What is the capital city of Lithuania?": [
+        "Vilnius", "Havana", "Budapest", "Prague"
+    ],
+    "What is the capital city of Indonesia?": [
+        "Jakarta", "Minsk", "Ankara", "Maputo"
+    ],
+    "What is the capital city of India?": [
+        "New Delhi", "Riyadh", "Manama", "Kuala Lumpur"
+    ],
+    "What is the capital city of Switzerland?": [
+        "Bern", "Zurich", "Belgrade", "Roseau" 
+    ],
+    "What is the capital city of Austria?": [
+        "Vienna", "Berlin", "Moroni", "Tallinn"
+    ]
 }
+
+
