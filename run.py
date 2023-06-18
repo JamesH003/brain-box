b7b1e19c7b9e6491736d8d86c140096be932a476
James Horgan <jamesrhorgan@gmail.com>
2023-06-18 12:27:23 +0000

    Refactor code into functions

--- run.py
+++ run.py
@@ -68,6 +68,30 @@ QUESTION_OPTIONS = {
 quantity_questions = min(QUESTION_QUANTITY_PER_GAME, len(QUESTION_OPTIONS))
 questions = random.sample(list(QUESTION_OPTIONS.items()), k=quantity_questions)
 
+# correct_answers = 0
+# for num, (question, options) in enumerate(questions, start=1):
+#     print(f"\nQuestion {num}:")
+#     print(f"{question}")
+#     correct_option = options[0]
+#     labeled_options = dict(
+#         zip(ascii_lowercase, random.sample(options, k=len(options)))
+#     )
+#     for label, option in labeled_options.items():
+#         print(f" {label}) {option}")
+
+#     while (answer_label := input("\nAnswer? \n")) not in labeled_options:
+#         print(f"Please answer one of {', '.join(labeled_options)}")
+
+#     answer = labeled_options[answer_label]
+#     if answer == correct_option:
+#         correct_answers += 1
+#         print("Correct! ✅")
+#     else:
+#         print(f"Wrong! ❌ The correct answer is {correct_option!r}")
+
+# print(f"\nYou got {correct_answers} question(s) right out of {num}! ")
+
+
 def launch_quiz():
 
     while (answer_label := input("\nAnswer? \n")) not in labeled_options:
