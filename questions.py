f53e24e845deefa2676fb82c0357ac1c48d08080
James Horgan <jamesrhorgan@gmail.com>
2023-06-21 10:44:36 +0000

    Add questions.py file and add currency questions

---
+++ questions.py
@@ -0,0 +1,125 @@
+QUESTIONS_CAPITALS = {
+    "What is the capital city of Portugal? 🇵🇹": [
+        "Lisbon", "Caracas", "Faro", "Ottawa"
+    ],
+    "What is the capital city of Denmark? 🇩🇰": [
+        "Copenhagen", "Moscow", "Tokyo", "Stockholm"
+    ],
+    "What is the capital city of Cuba? 🇨🇺": [
+        "Havana", "Jakarta", "Cairo", "Miami"
+    ],
+    "What is the capital city of Argentina? 🇦🇷": [
+        "Buenos Aires", "Budapest", "Monrovia", "Maseru"
+    ],
+    "What is the capital city of Croatia? 🇭🇷": [
+        "Zagreb", "Accra", "Damascus", "Split"
+    ],
+    "What is the capital city of Lithuania? 🇱🇹": [
+        "Vilnius", "Havana", "Budapest", "Prague"
+    ],
+    "What is the capital city of Indonesia? 🇮🇩": [
+        "Jakarta", "Minsk", "Ankara", "Maputo"
+    ],
+    "What is the capital city of India? 🇮🇳": [
+        "New Delhi", "Riyadh", "Manama", "Kuala Lumpur"
+    ],
+    "What is the capital city of Switzerland? 🇨🇭": [
+        "Bern", "Zurich", "Belgrade", "Roseau" 
+    ],
+    "What is the capital city of Austria? 🇦🇹": [
+        "Vienna", "Berlin", "Moroni", "Tallinn"
+    ],
+    "What is the capital city of Czech Republic? 🇨🇿": [
+        "Prague", "Kuwait", "Tirana", "Dublin"
+    ],
+    "What is the capital city of Egypt? 🇪🇬": [
+        "Cairo", "Madrid", "Rome", "Asmara"
+    ],
+    "What is the capital city of Barbados? 🇧🇧": [
+        "Bridgetown", "Niamey", "Nassau", "Roseau"
+    ],
+    "What is the capital city of Sweden? 🇸🇪": [
+        "Stockholm", "San Marino", "Oslo", "Valletta"
+    ],
+    "What is the capital city of Colombia? 🇨🇴": [
+        "Bogota", "Brasilia", "Kingston", "Tehran"
+    ],
+    "What is the capital city of Greece? 🇬🇷": [
+        "Athens", "Port-au-Prince", "Singapore", "Chișinău"
+    ],
+    "What is the capital city of Estonia? 🇪🇪": [
+        "Tallinn", "Baghdad", "Lisbon", "Nursultan"
+    ],
+    "What is the capital city of Germany? 🇩🇪": [
+        "Berlin", "Yaren district", "Munich", "Brussels"
+    ],
+     "What is the capital city of Slovakia? 🇸🇰": [
+        "Bratislava", "Kyiv", "Bucharest", "Athens"
+     ],
+     "What is the capital city of Ireland? 🇮🇪": [
+        "Dublin", "Cardiff", "Paris", "Cork"
+     ]
+}
+
+QUESTIONS_CURRENCIES = {
+    "What is the currency of Denmark?": [
+        "Krone", "Euro", "Lev", "Mark"
+    ],
+    "What is the currency of Czech Republic?": [
+        "Koruna", "Krone", "Yen", "Pound"
+    ],
+    "What is the currency of Hungary?": [
+        "Forint", "Euro", "Leu", "Lari"
+    ],
+    "What is the currency of Poland?": [
+        "Zloty", "Rupee", "Euro", "Dinar"
+    ],
+    "What is the currency of Norway?": [
+        "Krone", "Denar", "Euro", "Pound"
+    ],
+    "What is the currency of Iran?": [
+        "Rial", "Dinar", "Euro", "Dollar"
+    ],
+    "What is the currency of China?": [
+        "Yuan", "Dollar", "Won", "Yen"
+    ],
+    "What is the currency of North Korea?": [
+        "Won", "Rial", "Yuan", "Dollar"
+    ],
+    "What is the currency of Turkey?": [
+        "Lira", "Pound", "Rial", "Rupee" 
+    ],
+    "What is the currency of Australia?": [
+        "Dollar", "Pound", "Baht", "Peso"
+    ],
+    "What is the currency of UAE?": [
+        "Dirham", "Dollar", "Lira", "Franc"
+    ],
+    "What is the currency of Cuba?": [
+        "Peso", "Dollar", "Pound", "Krona"
+    ],
+    "What is the currency of Spain?": [
+        "Euro", "Pound", "Peso", "Franc"
+    ],
+    "What is the currency of Switzerland?": [
+        "Franc", "Euro", "Krona", "Pound"
+    ],
+    "What is the currency of Mexico?": [
+        "Peso", "Dollar", "Rial", "Baht"
+    ],
+    "What is the currency of Qatar?": [
+        "Riyal", "Rupee", "Pound", "Dirham"
+    ],
+    "What is the currency of Pakistan?": [
+        "Rupee", "Peso", "Dollar", "Lira"
+    ],
+    "What is the currency of Israel?": [
+        "Shekel", "Yen", "Dollar", "Euro"
+    ],
+     "What is the currency of Nepal?": [
+        "Rupee", "Pound", "Rial", "Won"
+     ],
+     "What is the currency of Iceland?": [
+        "Krona", "Euro", "Yen", "Dinar"
+     ]
+}
